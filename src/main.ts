11b0958dc129a1bcffb320b827b52c7702ef897b
Andry Gunawan <angun33@gmail.com>
2024-07-23 19:00:14 +1000

    Add fonts

--- src/main.ts
+++ src/main.ts
@@ -7,30 +7,44 @@ document.addEventListener('DOMContentLoaded', () => {
         targets: '#spiral-text',
         opacity: {
             value: [0, 1],
-            duration: 3000
+            duration: 3000,
+            easing: 'easeOutQuad',
         },
         scale: {
             value: [7, 1],
-            duration: 6000
+            duration: 6000,
+            easing: 'easeOutCubic',
         },
         rotate: {
             value: ['-75deg', '0deg'],
-            duration: 8000
+            duration: 8000,
+            easing: 'easeOutQuad',
         },
-        easing: 'easeOutQuad',
     });
     timeline.add({
-        targets: '.intro-text > span',
-        opacity: 1,
-        scale: [1.5, 1],
-        duration: 500,
-        delay: anime.stagger(300),
+        targets: '.header-text > span',
+        opacity: [0, 1],
+        scale: [1.2, 1],
+        duration: 700,
+        delay: anime.stagger(400),
         easing: 'easeOutQuad',
     }, '-=1000');
     timeline.add({
-        targets: 'hr',
+        targets: 'hr.top-line',
         opacity: [0, 1],
-        duration: 300,
+        duration: 400,
         easing: 'easeOutQuad',
-    }, '-=500')
+    }, '-=300');
+    timeline.add({
+        targets: '#search',
+        opacity: [0, 1],
+        duration: 400,
+        easing: 'easeOutQuad',
+    }, '-=300')
+    timeline.add({
+        targets: 'hr.bottom-line',
+        opacity: [0, 1],
+        duration: 400,
+        easing: 'easeOutQuad',
+    }, '-=300')
 }, false);
